21fc829605453dafae0b1acbcb7a98f154518bbd
pro-dev123 <coderank0816@gmail.com>
2023-04-23 00:55:51 +0900

    variable: ascii code

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,6 @@ int main(void)
 {
 	char variable; // type of 'variable' is character
 	variable = 'A'; // set value of 'variable' is 'A'
-	printf("%c", variable); // Print value of 'variable'
+	printf("%d", variable); // Print ascii value of 'variable'. 'A' is 65
 	return 0;
 }
