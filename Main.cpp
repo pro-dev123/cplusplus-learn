43ab37a30c0d7a105f3c2537596bdd62cb9e1fcc
pro-dev123 <129661435+pro-dev123@users.noreply.github.com>
2023-05-21 08:01:29 +0900

    Constructor

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,62 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(void)
+
+class Entity
 {
-	char variable; // type of 'variable' is character
-	variable = 'A'; // set value of 'variable' is 'A'
-	printf("%c", variable); // Print value of 'variable'
+public:
+	
+	Entity() // constructor
+	{
+		X = 0.0f;
+		Y = 0.0f;
+	}
+
+	Entity(float x, float y)
+	{
+		X = x;
+		Y = y;
+	}
+	
+	
+	float X, Y;
+
+	void Init()
+	{
+		X = 0.0f;
+		Y = 0.0f;
+	}
+
+	void Print()
+	{
+		std::cout << X << ", " << Y << std::endl;
+	}
+};
+
+
+class Logger
+{
+public:
+	Logger() = delete;
+
+	static void Log()
+	{
+		std::cout << "LOG" << std::endl;
+	}
+};
+
+
+int main()
+{
+	Entity e(10.0f, 5.0f);
+
+	// e.Init();
+	std::cout << e.X << std::endl;
+
+	e.Print();
+
+	Logger::Log();
+
+	std::cin.get();
+
 	return 0;
 }
